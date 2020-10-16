a88faebbfc62967b76e0961e56bf31b62cbf6ae0
ananyarout46057 <ananyarout46057@gmail.com>
2020-10-17 00:56:03 +0530

    Institute type components and Add dashboard , admin dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module'
+import { SharedModule } from './shared/shared.module';
+import { AdminDashboardComponent } from './Organization A/admin-dashboard/admin-dashboard.component';
+import { AddDashboardComponent } from './Organization A/add-dashboard/add-dashboard.component';
+import { EditInstituteTypeComponent } from './Organization A/Institute Type/edit-institute-type/edit-institute-type.component';
+import { InstituteTypeComponent } from './Organization A/Institute Type/institute-type/institute-type.component';
+import { ListInstituteTypeComponent } from './Organization A/Institute Type/list-institute-type/list-institute-type.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminDashboardComponent,
+    AddDashboardComponent,
+    EditInstituteTypeComponent,
+    InstituteTypeComponent,
+    ListInstituteTypeComponent
   ],
   imports: [
     BrowserModule,
