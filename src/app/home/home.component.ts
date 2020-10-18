5745fb91018c5f802517baae97eb5b935c890db2
Chandra Prakash Meher <b115022@iiit-bh.ac.in>
2020-10-18 08:18:22 +0000

    WIP_cognito

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth } from 'aws-amplify';
 
 @Component({
   selector: 'app-home',
@@ -8,20 +9,29 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  phone_number:number;
+  phone_number: number;
 
-  constructor(private router:Router) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  signIn =(event)=>{
-    console.log(event);
-    
-    console.log('my number======',this.phone_number);
-    if(this.phone_number)
-      {
+  signIn = (event) => {
+    let phoneNumber = "+91" + this.phone_number;
+    let otp = prompt(`OTP sent to your phone number to ${phoneNumber} ::${Date.now().toString().slice(-4)}`)
+
+    if (otp.length > 4) {
+      alert('Please enter 4 digit')
+    }
+    else {
+      Auth.signIn(phoneNumber).then(user => {
+        user = user;
+        console.log(user);
         this.router.navigate(['org'])
-      }
+      }).catch(err => {
+        alert(`${this.phone_number} is not registered as Super Admin.`)
+        this.router.navigate([''])
+      })
+    }
   }
 }
