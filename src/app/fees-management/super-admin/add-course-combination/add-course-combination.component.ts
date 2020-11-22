6c0f3b88bf60c0d4b0ae0f3e9f1b0cfcbac83d02
Chandra Prakash Meher <b115022@iiit-bh.ac.in>
2020-11-22 16:27:04 +0000

    Course combination

---
+++ src/app/fees-management/super-admin/add-course-combination/add-course-combination.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-course-combination',
+  templateUrl: './add-course-combination.component.html',
+  styleUrls: ['./add-course-combination.component.scss']
+})
+export class AddCourseCombinationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
